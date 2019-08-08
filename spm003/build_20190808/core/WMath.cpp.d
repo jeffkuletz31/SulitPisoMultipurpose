b65e489369e4e08657ace7243ba4042064287550
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2019-08-08 11:21:23 +0800

    20190808
    -rate is 1peso = 30s

---
+++ spm003/build_20190808/core/WMath.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\RHALFC~1\AppData\Local\Temp\arduino_build_255497\core\WMath.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WMath.cpp
---
+++ spm003/build_20190808/sketch/spm003.ino.cpp.d
@@ -0,0 +1,31 @@
+C:\Users\RHALFC~1\AppData\Local\Temp\arduino_build_255497\sketch\spm003.ino.cpp.o: \
+ C:\Users\RHALFC~1\AppData\Local\Temp\arduino_build_255497\sketch\spm003.ino.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ c:\program\ files\ (x86)\arduino\hardware\arduino\avr\variants\standard\pins_arduino.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\LiquidCrystal_I2C/LiquidCrystal_I2C.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Timer/Timer.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Timer/Timer.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Terminal/Terminal.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Buzzer/Buzzer.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\BillCoinAcceptor/BillCoinAcceptor.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Storage/Storage.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Protocol/Protocol.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Helper/Helper.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Button/Button.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\Device/Device.h \
+ C:\Users\rhalfcaacbay\Documents\Arduino\libraries\WatchDog/WatchDog.h
